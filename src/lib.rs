e4e590315be32abcf4a0d83d604900cd1cb5c3f8
Dónal Murray <donal.murray@cern.ch>
2021-04-13 08:52:57 +0100

    feat(parser): add whitespace support in expression

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,5 @@
 mod utils;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn parse_number() {
-        assert_eq!(Number::new("123"), ("", Number(123)));
-    }
-
-    #[test]
-    fn parse_add_op() {
-        assert_eq!(Op::new("+"), ("", Op::Add));
-    }
-
-    #[test]
-    fn parse_sub_op() {
-        assert_eq!(Op::new("-"), ("", Op::Sub));
-    }
-
-    #[test]
-    fn parse_mul_op() {
-        assert_eq!(Op::new("*"), ("", Op::Mul));
-    }
-
-    #[test]
-    fn parse_div_op() {
-        assert_eq!(Op::new("/"), ("", Op::Div));
-    }
-
-    #[test]
-    fn parse_one_plus_two() {
-        assert_eq!(
-            Expr::new("1+2"),
-            (
-                "",
-                Expr {
-                    lhs: Number(1),
-                    rhs: Number(2),
-                    op: Op::Add,
-                },
-            )
-        );
-    }
-}
-
 #[derive(Debug, PartialEq)]
 pub struct Number(pub i32);
 
@@ -89,9 +44,73 @@ pub struct Expr {
 impl Expr {
     pub fn new(s: &str) -> (&str, Self) {
         let (s, lhs) = Number::new(s);
+        let (s, _) = utils::extract_whitespace(s);
+
         let (s, op) = Op::new(s);
+        let (s, _) = utils::extract_whitespace(s);
+
         let (s, rhs) = Number::new(s);
 
         (s, Self { lhs, rhs, op })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_number() {
+        assert_eq!(Number::new("123"), ("", Number(123)));
+    }
+
+    #[test]
+    fn parse_add_op() {
+        assert_eq!(Op::new("+"), ("", Op::Add));
+    }
+
+    #[test]
+    fn parse_sub_op() {
+        assert_eq!(Op::new("-"), ("", Op::Sub));
+    }
+
+    #[test]
+    fn parse_mul_op() {
+        assert_eq!(Op::new("*"), ("", Op::Mul));
+    }
+
+    #[test]
+    fn parse_div_op() {
+        assert_eq!(Op::new("/"), ("", Op::Div));
+    }
+
+    #[test]
+    fn parse_one_plus_two() {
+        assert_eq!(
+            Expr::new("1+2"),
+            (
+                "",
+                Expr {
+                    lhs: Number(1),
+                    rhs: Number(2),
+                    op: Op::Add,
+                },
+            )
+        );
+    }
+
+    #[test]
+    fn parse_expr_with_whitespace() {
+        assert_eq!(
+            Expr::new("2 * 2"),
+            (
+                "",
+                Expr {
+                    lhs: Number(2),
+                    rhs: Number(2),
+                    op: Op::Mul,
+                },
+            ),
+        );
     }
 }
